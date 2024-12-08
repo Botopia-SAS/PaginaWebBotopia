427c57260c14ddc90776af322833a7705157979e
Botopia-SAS <botopia.sas@gmail.com>
2024-12-07 22:28:32 -0500

    NavBar actualizada para que se ponga borrosa
    El navBar se pone borrosa cuando se va bajando

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -85,7 +85,7 @@ const Navbar = () => {
   };
 
   return (
-    <header className="fixed top-0 left-0 right-0 z-50 bg-black-90">
+    <header className="fixed top-0 left-0 right-0 z-50 bg-black-90 backdrop-blur-sm">
       <div className="max-w-7xl mx-auto">
         <div className="flex items-center justify-between py-5 mx-auto c-space">
           <a
